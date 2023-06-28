0766d624892fa41f0ebc85d492557c24de0438bd
tokiimugi <tokiimugi@users.noreply.github.com>
2023-06-27 18:46:11 -0700

    error message and humidity + wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import tkinter as tk
+from tkinter import messagebox
 from config import API_KEY
 
 #API_URL = "https://api.openweathermap.org/data/2.5/weather"
@@ -32,10 +33,13 @@ def get_weather_data(city):
         wind_speed = data["current"]["wind_kph"] #data["wind"]["speed"]
 
         # Display the weather information
-        return [temperature,humidity,wind_speed]
+        return {"temperature" :temperature,
+                "humidity" : humidity,
+                "wind_speed": wind_speed}
 
     except requests.exceptions.RequestException as e:
         print(f"Error occurred: {e}")
+        messagebox.showerror("error", "try again")
 
 
 # GUI Application using Tkinter
@@ -51,11 +55,22 @@ class WeatherApp(tk.Tk):
 
         self.search_btn = tk.Button(self, text="Search", command=self.search_weather)
         self.search_btn.pack()
+        
+        self.temperature = tk.Label(text="Temperature: ")
+        self.humidity = tk.Label(text="Humidity: ")
+        self.wind_speed = tk.Label(text="Wind Speed: ")
+
+        self.temperature.pack()
+        self.humidity.pack()
+        self.wind_speed.pack()
 
     def search_weather(self):
         city = self.city_entry.get()
         weather = get_weather_data(city)
-        print(weather)
+        self.temperature["text"] = f"Temperature: {weather['temperature']}"
+        self.humidity["text"] = f"Humidity: {weather['humidity']}"
+        self.wind_speed["text"] = f"Wind Speed: {weather['wind_speed']}"
+
 
 if __name__ == "__main__":
     app = WeatherApp()
